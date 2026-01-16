a5621cfb009c1dd74d976e1af12cc3ae5487b63c
kevin8glez <kcipriang24@fpsanturtzilh.eus>
2026-01-16 09:02:15 +0100

    Erronka sormena

---
+++ erronkaFroga/src/main/java/com/example/demo/ErronkaFrogaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErronkaFrogaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErronkaFrogaApplication.class, args);
+	}
+
+}
