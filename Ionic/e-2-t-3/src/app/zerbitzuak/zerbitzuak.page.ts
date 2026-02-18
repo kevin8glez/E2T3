7f70e1976e9ab958d27335982188cc82b6d22db4
holed999 <egomarab24@fpsanturtzilh.eus>
2026-02-18 13:53:22 +0100

    clase 123

--- Ionic/e-2-t-3/src/app/zerbitzuak/zerbitzuak.page.ts
+++ Ionic/e-2-t-3/src/app/zerbitzuak/zerbitzuak.page.ts
@@ -16,49 +16,49 @@ export class ZerbitzuakPage {
       categoria: 'Oinarrizko zerbitzuak',
       icono: 'cut-outline',
       items: [
-        { nombre: 'Peinado', precio: '5 €', tiempo: '20 min', descripcion: 'Orrazkera edozein motatako' },
-        { nombre: 'Corte', precio: '4 €', tiempo: '15 min', descripcion: 'Mozketa klasikoa' },
+        { nombre: 'Orrazkera', precio: '5 €', tiempo: '20 min', descripcion: 'Edozein motatako orrazkera' },
+        { nombre: 'Mozketa', precio: '4 €', tiempo: '15 min', descripcion: 'Mozketa klasikoa' },
       ]
     },
     {
       categoria: 'Kolorea',
       icono: 'color-palette-outline',
-      nota: 'Peinado barne. Laka prezioan sartuta.',
+      nota: 'Orrazkera barne. Laka prezioan sartuta.',
       items: [
-        { nombre: 'Color corto', precio: '12 €', tiempo: '45 min', descripcion: 'Ile motzerako koloreaketa' },
-        { nombre: 'Color medio', precio: '14 €', tiempo: '50 min', descripcion: 'Ile ertainerako koloreaketa' },
-        { nombre: 'Color largo', precio: '16 €', tiempo: '55 min', descripcion: 'Ile luzerako koloreaketa' },
-        { nombre: 'Color extralargo', precio: '18 €', tiempo: '60 min', descripcion: 'Ile oso luzerako koloreaketa' },
+        { nombre: 'Kolore motza', precio: '12 €', tiempo: '45 min', descripcion: 'Ile motzerako koloreztatzea' },
+        { nombre: 'Kolore ertaina', precio: '14 €', tiempo: '50 min', descripcion: 'Ile ertainerako koloreztatzea' },
+        { nombre: 'Kolore luzea', precio: '16 €', tiempo: '55 min', descripcion: 'Ile luzerako koloreztatzea' },
+        { nombre: 'Kolore oso luzea', precio: '18 €', tiempo: '60 min', descripcion: 'Ile oso luzerako koloreztatzea' },
       ]
     },
     {
-      categoria: 'Mechak partzialak',
+      categoria: 'Mecha partzialak',
       icono: 'color-wand-outline',
       items: [
-        { nombre: 'Corto', precio: '7 €', tiempo: '40 min', descripcion: 'Mecha partzialak ile motzean' },
-        { nombre: 'Medio', precio: '9 €', tiempo: '45 min', descripcion: 'Mecha partzialak ile ertainean' },
-        { nombre: 'Largo', precio: '11 €', tiempo: '50 min', descripcion: 'Mecha partzialak ile luzean' },
-        { nombre: 'Extralargo', precio: '13 €', tiempo: '55 min', descripcion: 'Mecha partzialak ile oso luzean' },
+        { nombre: 'Motza', precio: '7 €', tiempo: '40 min', descripcion: 'Mecha partzialak ile motzean' },
+        { nombre: 'Ertaina', precio: '9 €', tiempo: '45 min', descripcion: 'Mecha partzialak ile ertainean' },
+        { nombre: 'Luzea', precio: '11 €', tiempo: '50 min', descripcion: 'Mecha partzialak ile luzean' },
+        { nombre: 'Oso luzea', precio: '13 €', tiempo: '55 min', descripcion: 'Mecha partzialak ile oso luzean' },
       ]
     },
     {
-      categoria: 'Mechak osoak',
+      categoria: 'Mecha osoak',
       icono: 'sparkles-outline',
       items: [
-        { nombre: 'Corto', precio: '15 €', tiempo: '70 min', descripcion: 'Mecha osoak ile motzean' },
-        { nombre: 'Medio', precio: '17 €', tiempo: '80 min', descripcion: 'Mecha osoak ile ertainean' },
-        { nombre: 'Largo', precio: '19 €', tiempo: '90 min', descripcion: 'Mecha osoak ile luzean' },
-        { nombre: 'Extralargo', precio: '21 €', tiempo: '100 min', descripcion: 'Mecha osoak ile oso luzean' },
+        { nombre: 'Motza', precio: '15 €', tiempo: '70 min', descripcion: 'Mecha osoak ile motzean' },
+        { nombre: 'Ertaina', precio: '17 €', tiempo: '80 min', descripcion: 'Mecha osoak ile ertainean' },
+        { nombre: 'Luzea', precio: '19 €', tiempo: '90 min', descripcion: 'Mecha osoak ile luzean' },
+        { nombre: 'Oso luzea', precio: '21 €', tiempo: '100 min', descripcion: 'Mecha osoak ile oso luzean' },
       ]
     },
     {
       categoria: 'Mechak + kolorea',
       icono: 'brush-outline',
       items: [
-        { nombre: 'Corto', precio: '25 €', tiempo: '90 min', descripcion: 'Mechak eta kolorea ile motzean' },
-        { nombre: 'Medio', precio: '27 €', tiempo: '100 min', descripcion: 'Mechak eta kolorea ile ertainean' },
-        { nombre: 'Largo', precio: '29 €', tiempo: '110 min', descripcion: 'Mechak eta kolorea ile luzean' },
-        { nombre: 'Extralargo', precio: '31 €', tiempo: '120 min', descripcion: 'Mechak eta kolorea ile oso luzean' },
+        { nombre: 'Motza', precio: '25 €', tiempo: '90 min', descripcion: 'Mechak eta kolorea ile motzean' },
+        { nombre: 'Ertaina', precio: '27 €', tiempo: '100 min', descripcion: 'Mechak eta kolorea ile ertainean' },
+        { nombre: 'Luzea', precio: '29 €', tiempo: '110 min', descripcion: 'Mechak eta kolorea ile luzean' },
+        { nombre: 'Oso luzea', precio: '31 €', tiempo: '120 min', descripcion: 'Mechak eta kolorea ile oso luzean' },
       ]
     }
   ];
@@ -73,3 +73,4 @@ export class ZerbitzuakPage {
     await this.hitzaurreaService.abrirModal();
   }
 }
+ 
